4918c077b24c194ecc63f7d11bca31713dba0bc5
Simao Gomes Viana <xdevs23@outlook.com>
2018-03-07 23:27:02 +0100

    Redo

--- db/db.js
+++ db/db.js
@@ -1,48 +1,186 @@
 
 const mkdir = require('mkdir-p')
 const fs = require('fs')
+var util = null
 
-module.exports = (dir = 'datastorage', structure = {}) => {
-  mkdir.sync(dir)
-  for (let tableName in structure) {
-    let table = structure[tableName]
-    for (let colName in table) {
-      mkdir.sync(`${dir}/${tableName}/${colName}`)
+const storage = {
+  tables: {
+
+  },
+  dir: '',
+  exists: false
+}
+
+const paths = {
+  tabledir (table) {
+    return `${storage.dir}/tables/${table}`
+  },
+  indexfile (table) {
+    return `${paths.tabledir(table)}/index`
+  },
+  tablefile (table) {
+    return `${paths.tabledir(table)}/db`
+  }
+}
+
+const index = {
+  tables: {}
+}
+
+function throwAndLog(msg) {
+  console.error(msg)
+  throw Error(msg)
+}
+
+function appendFile(file) {
+  log("Persisting data...")
+  let stream = fs.createWriteStream(file, { flags: 'a' })
+  return {
+    append (content) {
+      stream.write(content)
+    },
+    async close () {
+      stream.end()
+      log("...done")
     }
   }
-  let dbobj
-  dbobj = {
-    insert (table, data) {
-      let newId = 0
-      for (let key in data) {
-        let folder = `${dir}/${table}/${key}`
-        mkdir.sync(folder)
-        if (newId === 0) {
-          let files = fs.readdirSync(folder)
-          let sortedFiles = files.sort((a, b) => b - a)
-          if (sortedFiles.length === 0) {
-            newId = 1
-          } else {
-            newId = Number.parseInt(sortedFiles[0])
-            if (newId === NaN) {
-              throw Error(`NaN can't be used as key`)
-            }
-            newId++
-          }
-        }
-        let finalFile = `${folder}/${newId}`
-        fs.writeFileSync(finalFile, JSON.stringify(data[key]))
+}
+
+function log(msg) {
+  console.log(`[ezpzdb] ${msg}`)
+  return msg
+}
+
+function logi(obj) {
+  if (util === null) util = require('util')
+  return log(util.inspect(obj))
+}
+
+function readFilePartialSync (filename, start, length) {
+  var buf = new Buffer(length)
+  var fd = fs.openSync(filename, 'r')
+  fs.readSync(fd, buf, 0, length, start)
+  fs.closeSync(fd)
+  return buf
+}
+
+module.exports = (dir = 'datastorage') => {
+  log("Easy Peasy Lemon Squeezy Databasey")
+  log("Here to serve.")
+  log("")
+  log("Initializing...")
+  mkdir.sync(dir)
+  storage.dir = dir
+  // This file is used to indicate that the
+  // database has already been created and is ready to use
+  paths.stamp = `${storage.dir}/.stamp`
+  if (storage.exists = fs.existsSync(paths.stamp)) {
+    log(`Database exists, reading index into memory...`)
+    let tableNames = fs.readdirSync(paths.tabledir(''))
+    tableNames.forEach(tableName => {
+      index.tables[tableName] =
+        JSON.parse(fs.readFileSync(paths.indexfile(tableName)))
+      storage.tables[tableName] = {
+        lastId: index.tables[tableName].lastId,
+        items: []
       }
-      return newId
+    })
+  } else {
+    log(`New database, will be created once data is persisted`)
+  }
+  let dbobj = {
+    createTable (table) {
+      storage.tables[table] = {
+        lastId: 0,
+        items: []
+      }
+    },
+    persistData () {
+      return new Promise((resolve, reject) => {
+        if (!storage.exists) {
+          log("Now it's time to create a new database")
+          fs.writeFile(paths.stamp, `${Date.now()}`, err => {
+            if (err) reject(err)
+          })
+          storage.exists = true
+        }
+        for (let tableName in storage.tables) {
+          let table = storage.tables[tableName]
+          mkdir.sync(paths.tabledir(tableName))
+          if (!index.tables[tableName]) {
+            index.tables[tableName] = { index: {} }
+          }
+          index.tables[tableName].lastId = table.lastId
+          let offset = 0
+          let file = appendFile(paths.tablefile(tableName))
+          for (entryIx in table.items) {
+            let entry = table.items[entryIx]
+            // Skip already existing entries
+            if (index.tables[tableName].index[`${entry.id}`]) {
+              continue
+            }
+            // Start off with the last offset + its entry's length
+            else if (Object.keys(index.tables[tableName].index).length > 0) {
+              let lastItem = index.tables[tableName].index[`${table.beforeLastId}`]
+              offset = lastItem.o + lastItem.l
+            }
+            // Create the string to save the data
+            let jsonText = JSON.stringify(entry)
+            // Create an index entry so that we can address this quickly
+            index.tables[tableName].index[`${entry.id}`] = {
+              o: offset,
+              l: jsonText.length
+            }
+            // Append the entry to the file
+            file.append(jsonText)
+            // And now grow the offset to continue
+            offset += jsonText.length
+          }
+          file.close()
+          fs.writeFile(paths.indexfile(tableName),
+            JSON.stringify(index.tables[tableName]), err => {
+              if (err) {
+                reject(err)
+              } else {
+                // Now that the data is persisted, remove it from memory
+                table.items = []
+              }
+            }
+          )
+        }
+      })
+    },
+    insert (table, data) {
+      if (!storage.tables[table]) {
+        dbobj.createTable(table)
+      }
+      let tbl = storage.tables[table]
+      tbl.beforeLastId = tbl.lastId
+      data.id = ++tbl.lastId
+      tbl.items.push(data)
+      dbobj.persistData().then(() => {}, e => throwAndLog(e))
+      return tbl.lastId
     },
     update (table, data) {
       if (data.id === undefined || data.id === 0) {
         throw Error('id must exist and be at least 1')
       }
-      for (let key in data) {
-        fs.writeFileSync(
-          `${dir}/${table}/${key}/${data.id}`, JSON.stringify(data[key]))
+      let indexItem = index.tables[table].index[`${data.id}`]
+      if (!indexItem) {
+        return {}
       }
+      let filename = paths.tablefile(table)
+      let newData = JSON.stringify(data)
+      let lastItem = index.tables[table]
+        .index[`${storage.tables[table].lastId}`]
+      indexItem.o = lastItem.o + lastItem.l
+      indexItem.l = newData.length
+      let file = appendFile(filename)
+      file.append(newData)
+      file.close()
+      fs.writeFileSync(paths.indexfile(table),
+        JSON.stringify(index.tables[table]))
+      return dbobj.get(table, data.id)
     },
     remove (table, id) {
       if (id === undefined || id === 0) {
@@ -58,43 +196,29 @@ module.exports = (dir = 'datastorage', structure = {}) => {
       }
       return true
     },
-    get (table, id, args = null) {
+    get (table, id) {
       if (id === undefined || id === 0) {
         throw Error('id must exist and be at least 1')
       }
-      let tableFolder = `${dir}/${table}`
-      let result = { id }
-      if (args === null) {
-        let args = fs.readdirSync(tableFolder)
+      let indexEntry = index.tables[table].index[`${id}`]
+      if (!indexEntry) {
+        return {}
       }
-      for (let arg in args) {
-        result[arg] = JSON.parse(
-            fs.readFileSync(`${tableFolder}/${arg}/${id}`))
-      }
-      return result
+      return JSON.parse(
+        readFilePartialSync(paths.tablefile(table),
+        indexEntry.o, indexEntry.l))
     },
     getAll (table) {
       let result = []
-      let tableFolder = `${dir}/${table}`
-      let folders = fs.readdirSync(tableFolder)
-      let sortedFiles = fs.readdirSync(
-        `${tableFolder}/${folders[0]}`).sort((a, b) => b - a)
-      let lastId = Number.parseInt(sortedFiles[0])
-      for (let i = 1; i <= lastId; i++) {
-        let item = { id: i }
-        let add = true
-        for (let folderIx in folders) {
-          let folder = folders[folderIx]
-          let toGet = `${tableFolder}/${folder}/${i}`
-          if (!fs.existsSync(toGet)) {
-            add = false
-            break
-          }
-          item[folder] = JSON.parse(
-            fs.readFileSync(toGet))
-        }
-        if (add) result.push(item)
+      let tableIndex = index.tables[table].index
+      var fd = fs.openSync(paths.tablefile(table), 'r');
+      for (let tableIndexIx in tableIndex) {
+        let entry = tableIndex[tableIndexIx]
+        var buf = new Buffer(entry.l);
+        fs.readSync(fd, buf, 0, entry.l, entry.o);
+        result.push(JSON.parse(buf))
       }
+      fs.close(fd);
       return result
     }
   }
